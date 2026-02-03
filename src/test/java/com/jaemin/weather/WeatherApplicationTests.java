ed885bcf366773fd428a2ca8919b3385389cb020
shangpa <kjm9049123@gmail.com>
2026-02-03 16:48:12 +0900

    테스트 코드로 대신 실행

--- src/test/java/com/jaemin/weather/WeatherApplicationTests.java
+++ src/test/java/com/jaemin/weather/WeatherApplicationTests.java
@@ -1,13 +1,19 @@
 package com.jaemin.weather;
 
+import com.jaemin.weather.service.WeatherService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WeatherApplicationTests {
 
+    @Autowired
+    private WeatherService weatherService;
+
     @Test
     void contextLoads() {
+        weatherService.fetchAndSaveWeather(); // 실행 후 자동 종료됨
     }
 
 }
